8be11bf03834763d3be1020c34b7683749e3c4a7
Rushil Shah <rushilrs@usc.edu>
2025-10-18 00:13:02 -0400

    [any message]

--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, _ = server_socket.accept() # wait for client
+
+    connection.sendall(b"+PONG\r\n")
+    connection.close()
 
 
 if __name__ == "__main__":
